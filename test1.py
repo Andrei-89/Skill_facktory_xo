e1d8d36786defe5b5d6cb49851ab534595742d46
Andrei-89 <124354220+Andrei-89@users.noreply.github.com>
2023-03-31 23:04:11 +0400

    первый

---
+++ test1.py
@@ -0,0 +1,16 @@
+text = "fifa1988"
+for i in range(10, 99):
+    while str(i) in text:
+        if 2000 <= 2000+i <= 2016:
+            print(2000+i-1)
+
+        if 1986 <= 1900+i <= 1999:
+            print(1900 + i - 1)
+        break
+    # for j in year:
+    #     if i == j:
+    #         print(i, end="")
+
+
+# import pandas as pd
+# def year(row)
